1ea93c3cc53d81cf3d0ca4918985ce803438806b
orangain <orangain@gmail.com>
2013-04-01 22:41:04 +0900

    Setup deploy. Need more configuration for eg. restart.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,12 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :stages, %w(staging production)
+set :default_stage, "production"
+require 'capistrano/ext/multistage'
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+set :application, "mydashboard"
+set :repository,  "https://github.com/orangain/mydashboard"
+
+set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
