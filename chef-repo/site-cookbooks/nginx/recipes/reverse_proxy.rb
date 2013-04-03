329e7b8d3ed83e4fdbffe252f95c39edf86992b1
orangain <orangain@gmail.com>
2013-04-03 22:15:30 +0900

    Added resource to enable reverse_proxy and disable default site.

--- chef-repo/site-cookbooks/nginx/recipes/reverse_proxy.rb
+++ chef-repo/site-cookbooks/nginx/recipes/reverse_proxy.rb
@@ -6,3 +6,18 @@ template "reverse_proxy_site.conf" do
   mode "0644"
   notifies :reload, 'service[nginx]'
 end
+
+# Disable default site
+link "/etc/nginx/sites-enabled/default" do
+  action :delete
+  only_if "test -L /etc/nginx/sites-enabled/default"
+  notifies :reload, 'service[nginx]'
+end
+
+# Enable reverse proxy
+link "/etc/nginx/sites-enabled/reverse_proxy" do
+  action :create
+  to "/etc/nginx/sites-available/reverse_proxy"
+  owner "root"
+  notifies :reload, 'service[nginx]'
+end
